b5b07da797109ace1406da4ad4f0c2dc2248e9fa
youngentry <gentry_@naver.com>
2022-07-19 17:52:06 +0900

    220719 2kgames

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 $(function () {
-  $("body").css("background", "#fff");
+  $(".slider").slick({
+    arrows: false,
+    fade: true,
+    swipe: false,
+  });
+  $(".titleSlider").slick({
+    asNavFor: ".slider",
+    arrows: false,
+    slidesToShow: 1,
+    centerMode: true,
+    centerPadding: "480px",
+    focusOnSelect: true,
+    swipeToSlide: true,
+    cssEase: "ease-in-out",
+  });
+
+  $(".title.slick-current").on("click", function () {
+    $(".slider .info").toggleClass("on");
+  });
 });
