185e1e6fdb278212a5fc0ca88724cfd8cd2ed455
youngentry <gentry_@naver.com>
2022-07-21 11:23:45 +0900

    220721 mainVisual 얇은 실선, 화살표

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(function () {
+  // .mainVisual;;
   $(".slider").slick({
     arrows: false,
     fade: true,
@@ -15,65 +16,26 @@ $(function () {
     cssEase: "ease-in-out",
   });
 
-  ////////////////////////////////////////////////////
-
-  // $("h1").on("click", function () {
-  //   console.log(num, "얘는22222222 num");
-  // });
-
-  // $(".title.quarryTitle[aria-hidden='false']").on("click", function () {
-  //   $(".infoWrap").addClass("on");
-  // });
-
-  //////////////////////////////////////////////////
-
-  // var TUL = $(".titleSlider img");
-  // console.log(TUL);
-  // TUL.each((idx, el) => {
-  //   console.log(idx, el);
-  // });
-  // console.log($(".description .quarryInfo"));
-  // $(".titleSlider").on("afterChange", function (e, s, c, n) {
-  //   console.log($(".mainBg")[c]);
-  //   console.log($(".title")[c + 2]);
-  //   console.log($(".info")[c]);
-
-  // var title = $(".titleSlider .title")[c + 2];
-  // var description = $(".description .info")[c];
-
-  // title.on("click", function () {
-  //   $(".infoWrap").addClass("on");
-  //   description.addClass("on");
-  // });
-  // $(".description .closeBtn").on("click", function () {
-  //   $(".infoWrap").removeClass("on");
-  //   description.removeClass("on");
-  // });
   var num = 0;
-
   $(".slider").on("afterChange", function (e, s, c) {
     num = c;
   });
-  $(".infoWrap").on("click", function () {
-    $(".infoWrap").removeClass("on");
-    $(".description .info").removeClass("on");
-    if (!$(".infoWrap").hasClass("on")) {
-      for (i = 0; i < 4; i++) {
-        $("iframe")[i].contentWindow.postMessage('{"event":"command","func":"' + "stopVideo" + '","args":""}', "*");
+  function descriptionControl(className) {
+    $(className).on("click", function () {
+      $(".infoWrap").removeClass("on");
+      $(".description .info").removeClass("on");
+      if (!$(".infoWrap").hasClass("on")) {
+        for (i = 0; i < 4; i++) {
+          $("iframe")[i].contentWindow.postMessage('{"event":"command","func":"' + "stopVideo" + '","args":""}', "*");
+        }
       }
-    }
-  });
-  $(".description .closeBtn").on("click", function () {
-    $(".infoWrap").removeClass("on");
-    $(".description .info").removeClass("on");
-    if (!$(".infoWrap").hasClass("on")) {
-      for (i = 0; i < 4; i++) {
-        $("iframe")[i].contentWindow.postMessage('{"event":"command","func":"' + "stopVideo" + '","args":""}', "*");
-      }
-    }
-  });
+    });
+  }
+  descriptionControl(".infoWrap");
+  descriptionControl(".description .closeBtn");
   $(".title img").on("click", function () {
     $(".infoWrap").addClass("on");
     $(".info").eq(num).addClass("on");
   });
+  // .mainVisual;;
 });
